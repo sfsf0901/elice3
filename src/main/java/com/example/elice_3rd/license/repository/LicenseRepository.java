dd29013f8467261d0e85de182638c398b2f398ee
rudcjs <rudcjs335@gmail.com>
2025-02-05 16:30:41 +0900

    feat: License CRUD 구현

--- src/main/java/com/example/elice_3rd/license/repository/LicenseRepository.java
+++ src/main/java/com/example/elice_3rd/license/repository/LicenseRepository.java
@@ -4,6 +4,9 @@ import com.example.elice_3rd.license.entity.License;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LicenseRepository extends JpaRepository<License, Long> {
+    Optional<License> findByEmail(String email);
 }
--- src/main/java/com/example/elice_3rd/license/service/LicenseService.java
+++ src/main/java/com/example/elice_3rd/license/service/LicenseService.java
@@ -1,4 +1,26 @@
 package com.example.elice_3rd.license.service;
 
+import com.example.elice_3rd.license.entity.License;
+import com.example.elice_3rd.license.repository.LicenseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class LicenseService {
+    private final LicenseRepository licenseRepository;
+
+    public void createLicense(License license){
+        licenseRepository.save(license);
+    }
+
+    // TODO entity 존재하지 않을 때 예외 처리
+    public License getLicense(String email){
+        return licenseRepository.findByEmail(email).orElseThrow();
+    }
+
+    public void delete(String email){
+        License license = licenseRepository.findByEmail(email).orElseThrow();
+        licenseRepository.delete(license);
+    }
 }
