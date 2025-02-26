d133e63d402f03269c68516ef25d691ee8e8e7e4
rudcjs <rudcjs335@gmail.com>
2025-02-26 17:38:15 +0900

    fix: TODO list 해결

--- src/main/java/com/example/elice_3rd/security/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/elice_3rd/security/oauth2/OAuth2LoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.example.elice_3rd.security.oauth2;
 
+import com.example.elice_3rd.common.exception.NoSuchDataException;
 import com.example.elice_3rd.member.repository.MemberRepository;
 import com.example.elice_3rd.security.CustomUserDetails;
 import com.example.elice_3rd.security.MemberDetail;
@@ -40,12 +41,12 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         else if(userInfo.get("response") != null)
             userInfo = (Map<String, Object>) userInfo.get("response");
 
-        // TODO email null check 필요
-        String email = userInfo.get("email").toString();
-        String role = memberRepository.findByEmail(email).orElseThrow().getRole().getKey();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        log.error(objectMapper.writeValueAsString(userInfo));
+        String email = "";
+        if(userInfo.get("email") != null)
+            email = userInfo.get("email").toString();
+        String role = memberRepository.findByEmail(email).orElseThrow(() ->
+                new NoSuchDataException("회원 조회 실패: 이메일과 일치하는 회원이 존재하지 않습니다.")
+        ).getRole().getKey();
 
         String accessToken = jwtUtil.createAccessToken(email, role);
         String refreshToken = jwtUtil.createRefreshToken(email, role);
