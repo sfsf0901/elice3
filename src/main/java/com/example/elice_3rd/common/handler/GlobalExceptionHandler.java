88d3665bed8d7ea843e2e11f9cc92cc807266796
rudcjs <rudcjs335@gmail.com>
2025-02-26 17:31:52 +0900

    feat: EntityNotFoundException,       IllegalArgumentException,       RuntimeException handler 추가

--- src/main/java/com/example/elice_3rd/common/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/elice_3rd/common/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.elice_3rd.common.ErrorCode;
 import com.example.elice_3rd.common.ErrorResponse;
 import com.example.elice_3rd.common.exception.NoSuchDataException;
 import com.example.elice_3rd.common.exception.UnauthorizedException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,18 +23,41 @@ import java.util.Map;
 public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<ErrorResponse> handleUnauthorized(UnauthorizedException e){
+        log.error("UnauthorizedException : {}", e.getMessage());
         final ErrorResponse response = ErrorResponse.of(ErrorCode.UNAUTHORIZED_ERROR, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(NoSuchDataException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchData(NoSuchDataException e) {
+        log.error("NoSuchDataException : {}", e.getMessage());
         final ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND_ERROR, e.getMessage());
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgument(IllegalArgumentException e) {
+        log.error("IllegalArgumentException : {}", e.getMessage());
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.BAD_REQUEST_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEntityNotFound(EntityNotFoundException e) {
+        log.error("EntityNotFoundException : {}", e.getMessage());
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_FOUND_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException : {}", e.getMessage());
+        final ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
         log.error("handleMethodArgumentNotValidException", e);
         final ErrorResponse response = ErrorResponse.of(ErrorCode.NOT_VALID_ERROR, toBodyResponse(e));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
